77da0216e1ae8af6ea35d6a85a444b3cf434eb4f
chandraitfc <chandra@itforchange.net>
2026-01-08 11:25:02 +0530

    moved models to files

--- apps/sdz/sdz.py
+++ apps/sdz/sdz.py
@@ -15,7 +15,7 @@ from ela import util
 # disable metrics for current session
 set_telemetry_metrics(False)
 pipeline = Pipeline.from_pretrained(
-    "/apps/sdz/models/models--pyannote--speaker-diarization-community-1/snapshots/3533c8cf8e369892e6b79ff1bf80f7b0286a54ee")
+    "/apps/files/models/pyannote/models--pyannote--speaker-diarization-community-1/snapshots/3533c8cf8e369892e6b79ff1bf80f7b0286a54ee")
 
 
 def normalize_mfcc(mfcc_features):
