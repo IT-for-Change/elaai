aa9b77b774b40c28c861524a3fa0b194aacb36ec
chandraitfc <chandra@itforchange.net>
2026-01-09 09:11:31 +0530

    stt implementation feature complete for this iteration

--- apps/stt/app.py
+++ apps/stt/app.py
@@ -3,43 +3,50 @@ import requests
 import tempfile
 import os
 import json
+from loguru import logger
 from ela import client as ela_client
 from ela import util as ela_util
-from stt import transcribe
+from stt.stt import transcribe
 
 OPERATION = "stt"
 
 
 def main(activity_id):
 
+    logger.info('Initializing client')
     api_client = ela_client.Client()
+    logger.info('Client get_data')
     response = api_client.get_data(activity_id, OPERATION)
     data = response.json()
+    logger.info('Received response')
 
     # important to maintain live context using 'with' for temp directories/files
     with tempfile.TemporaryDirectory() as tmp_dir:
         # download all audio files first, then transcribe in a loop
-        downloads = ela_util.do_download(tmp_dir)
+        downloads = ela_util.do_download(data, tmp_dir, OPERATION)
 
         outputs = []
 
-        # after all audio files downloaded, run diarize for each
+        # after all audio files downloaded, run transcribe for each
         for d in downloads:
-            asr_text, language, confidence = diarize(
+            logger.info(
+                f'Performing transcription for submission {d["item_key"]} and entry {d["entry_key"]} in language {d["language"]}')
+            transcription_output = transcribe(
                 d["audio_path"], d["language"])
+            logger.info(
+                f'Completed transcription for submission {d["item_key"]} and entry {d["entry_key"]}')
+            logger.info(transcription_output)
             outputs.append({
                 "item_key": d["item_key"],
                 "entry_key": d["entry_key"],
-                "asr_text": asr_text,
-                "language": language,
-                "confidence": confidence
+                "stt": transcription_output
             })
 
-        payload = {
-            "outputs": outputs
-        }
-        print(payload)
-        api_client.put_data(payload)
+            if len(outputs) > 1:
+                break
+
+        print(outputs)
+        api_client.put_data(outputs, OPERATION)
 
 
 if __name__ == "__main__":
--- apps/stt/stt.py
+++ apps/stt/stt.py
@@ -1,5 +1,139 @@
-import whisper
+import string
+import os
+from collections import Counter
+import torch
+import librosa
+from transformers import WhisperProcessor, WhisperForConditionalGeneration
+
+processor = WhisperProcessor.from_pretrained(
+    "/apps/files/models/whisper/large-v3-turbo")
+model = WhisperForConditionalGeneration.from_pretrained(
+    "/apps/files/models/whisper/large-v3-turbo")
+
+device = torch.device("cpu")
+model = model.to(device)
+
+hallu_thresholds_definition = {
+    1: 5,  # Unigrams - occuring 5 or more times
+    2: 5,  # Bigrams - occuring 5 or more times
+    3: 3,  # Trigrams - occuring 3 or more times
+    4: 3,  # Phrases with 4 words - occuring 3 or more times
+    5: 2,  # Phrases with 5 or more words - occurring 2 or more times
+    6: 2,  # Phrases with 6 or more words - occurring 2 or more times
+    7: 2,  # Phrases with 7 or more words - occurring 2 or more times
+    8: 2,  # Phrases with 8 or more words - occurring 2 or more times
+    9: 2,  # Phrases with 9 or more words - occurring 2 or more times
+    10: 2,  # Phrases with 10 or more words - occurring 2 or more times
+}
 
 
-def transcribe(audio, language):
-    return "hello this is transcribed text!", "en", "100"
+def hallucination_metrics(text):
+    hallu_score, hallu_tokens = detect_hallucination(text)
+    return hallu_score, hallu_tokens
+
+
+def get_n_gram_sets(tokens):
+    max_n_gram_sets_to_make = len(hallu_thresholds_definition)
+    # Step 2: Generate n-grams for each n from 1 to max_n_gram
+    ngrams = {}
+    for n in range(1, max_n_gram_sets_to_make + 1):
+        ngram_list = []
+        i = 0
+        while i + n <= len(tokens):  # Ensure we don't go out of bounds
+            # Join words in the current window to form the n-gram
+            ngram = ' '.join(tokens[i:i + n])
+            ngram_list.append(ngram)
+            i += n  # Move the index forward by N to ensure non-overlapping N-grams
+        ngrams[n] = ngram_list
+
+    return ngrams
+
+
+def check_n_gram_repetition(n, n_gram_set):
+    n_gram_token_count = len(n_gram_set)
+    threshold_for_n = hallu_thresholds_definition[n]
+    hallucination_tokens = []
+    is_hallu = False
+
+    # Loop through the array to find consecutive occurrences
+    count = 1  # counter to count consecutive tokens
+    for i in range(1, n_gram_token_count):
+        if n_gram_set[i] == n_gram_set[i - 1]:  # Check if current token equals previous
+            count += 1
+            hallucination_tokens.append(n_gram_set[i])
+        else:
+            count = 1  # Reset count when the sequence breaks
+
+        if count >= threshold_for_n:  # If we hit threshold number of consecutive matches
+            is_hallu = True
+
+    return is_hallu, hallucination_tokens
+
+
+def tokens_from_text(text):
+    translator = str.maketrans('', '', string.punctuation)
+    tokens = text.translate(translator).lower().split()
+    return tokens
+
+
+def detect_hallucination(text):
+
+    total_hallu_tokens = 0
+    max_n_gram_sets_to_make = len(hallu_thresholds_definition)
+    tokens = tokens_from_text(text)
+
+    ngram_sets = get_n_gram_sets(tokens)
+
+    for n in range(1, max_n_gram_sets_to_make + 1):
+        n_gram_set = ngram_sets[n]
+        is_hallu, hallu_tokens = check_n_gram_repetition(n, n_gram_set)
+        if is_hallu:
+            total_hallu_tokens = sum(len(s.split()) for s in hallu_tokens)
+            # next, apply correction factor to include the first occurrence since hallucination is confirmed
+            total_hallu_tokens += n
+            # this breaking out at the first n-gram match for hallucination makes the assumption
+            # that there are not goingto be different regions of hallucinations in the
+            # transcription involving different sets of word tokens. assumption to be revisited.
+            break
+
+    hallu_score = (total_hallu_tokens * 100) // len(tokens)
+    return hallu_score, hallu_tokens
+
+
+def guess_hallucination_text(asr_text):
+    return ''
+
+
+def transcribe(audio_file, language):
+
+    audio, sr = librosa.load(audio_file, sr=16000)
+
+    inputs = processor(
+        audio,
+        sampling_rate=16000,
+        return_tensors="pt"
+    )
+
+    input_features = inputs.input_features.to(device)
+
+    with torch.no_grad():
+        predicted_ids = model.generate(
+            input_features,
+            task="transcribe",
+            language=language
+        )
+
+    asr_text = processor.batch_decode(
+        predicted_ids,
+        skip_special_tokens=True
+    )[0]
+
+    hallu_score, hallu_text = hallucination_metrics(asr_text)
+
+    transcription_output = {
+        'asr_text': asr_text,
+        'hallu_score': hallu_score,
+        'hallu_text': hallu_text
+    }
+
+    return {"transcription_output": transcription_output}
